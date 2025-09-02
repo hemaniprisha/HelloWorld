ee9f0f1ac8bc6d81ae75de8a74cf9b4957617b67
Prisha Hemani <hemaniprisha1@gmail.com>
2025-09-01 17:46:12 -0700

    Initial commit with working app

---
+++ app/src/main/java/com/example/helloworld2/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.helloworld2
+
+import android.os.Bundle
+import android.widget.Button
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.StartButton)
+        button.setOnClickListener {
+            Log.v("Hello world", "Button Clicked!")
+            Toast.makeText(this, "General Kenobi!", Toast.LENGTH_SHORT).show()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
